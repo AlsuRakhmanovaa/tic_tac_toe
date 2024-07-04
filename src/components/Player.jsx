d513c21c3fdb16a7870ba4fa3f471d9cf4a67e13
Alsu <rakhmanova_2003@inbox.ru>
2024-07-04 14:03:36 +0300

    players' rename, game board, players' turn

---
+++ src/components/Player.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let editiblePlayerName = <span className="player-name">{playerName}</span>;
+
+  if (isEditing) {
+    editiblePlayerName = (
+      <input type="text" required value={playerName} onChange={handleChange} />
+    );
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {editiblePlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+    </li>
+  );
+}
